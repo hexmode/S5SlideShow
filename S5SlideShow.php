a26127089853914519afc88c0473195c415454c9
Mark A. Hershberger <mah@nichework.com>
2020-01-02 12:33:01 -0500

    Modernising code style
    The easy, automatic stuff that codesniffer can do.

--- S5SlideShow.php
+++ S5SlideShow.php
@@ -23,17 +23,18 @@
 
 /**
  * @author Vitaliy Filippov <vitalif@mail.ru>
- * @author Wolfgang Fahl 
+ * @author Wolfgang Fahl
  * @package MediaWiki
  * @subpackage Extensions
  */
 
-if (!defined('MEDIAWIKI'))
-    die();
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
 
-$dir = dirname(__FILE__);
+$dir = __DIR__;
 
-//--- Default configuration ---//
+// --- Default configuration ---//
 
 // Headings with this text will be treated as slide headings
 // false = do not treat subsections as slides by default
@@ -46,7 +47,7 @@ $egS5SlideIncMark = '\(step\)';
 $egS5SlideCenterMark = '\(center\)';
 
 // Filesystem path to slideshow template file
-$egS5SlideTemplateFile = $dir.'/slide.htm';
+$egS5SlideTemplateFile = $dir . '/slide.htm';
 
 // In scaled slideshow mode, images are scaled proportionally
 // with all other elements. This means you can set image size
@@ -62,17 +63,17 @@ $egS5SlideTemplateFile = $dir.'/slide.htm';
 // downsampled thumbnails. (default = true)
 $egS5BrowserScaleHack = true;
 
-//Default Style for presentations. Useful for not redefining 'default' style
+// Default Style for presentations. Useful for not redefining 'default' style
 //but change all presentations by default to some "corporate" style.
 $egS5DefaultStyle = 'default';
 
-//Should Slides be scaled by default?
+// Should Slides be scaled by default?
 $egS5Scaled = false;
 
-//--- End configuration ---//
+// --- End configuration ---//
 $wgExtensionFunctions[] = 'S5SlideShowHooks::Setup';
 
-/* legacy Extension setup 
+/* legacy Extension setup
 
 $wgExtensionMessagesFiles['S5SlideShow'] = $dir.'/S5SlideShow.i18n.php';
 $wgHooks['UnknownAction'][] = 'S5SlideShowHooks::UnknownAction';
@@ -85,191 +86,171 @@ $wgHooks['MagicWordwgVariableIDs'][] = 'S5SlideShowHooks::MagicWordwgVariableIDs
 $wgHooks['ParserGetVariableValueSwitch'][] = 'S5SlideShowHooks::ParserGetVariableValueSwitch';
 */
 
-class S5SlideShowHooks
-{
-    static $styles = array(
-        'core.css'    => 's5-core.css',
-        'base.css'    => 's5-base.css',
-        'framing.css' => 's5-framing.css',
-        'pretty.css'  => '$skin/pretty.css',
-    );
-    static $parsingSlide = false;
+class S5SlideShowHooks {
+	static $styles = [
+		'core.css'    => 's5-core.css',
+		'base.css'    => 's5-base.css',
+		'framing.css' => 's5-framing.css',
+		'pretty.css'  => '$skin/pretty.css',
+	];
+	static $parsingSlide = false;
 
-    // Setup parser hooks for S5
-    static function ParserFirstCallInit($parser)
-    {
-        if (!isset($parser->extS5Hooks))
-        {
-            $parser->setHook('slideshow', 'S5SlideShow::slideshow_view');
-            $parser->setHook('slide', 'S5SlideShow::slideshow_legacy');
-            $parser->setHook('slides', 'S5SlideShow::slides_view');
-            $parser->setHook('slidecss', 'S5SlideShow::slidecss_view');
-        }
-        elseif ($parser->extS5Hooks == 'parse')
-        {
-            $parser->setHook('slideshow', array($parser->extS5, 'slideshow_parse'));
-            $parser->setHook('slide', array($parser->extS5, 'slideshow_parse'));
-            $parser->setHook('slides', 'S5SlideShow::empty_tag_hook');
-            $parser->setHook('slidecss', 'S5SlideShow::empty_tag_hook');
-        }
-        elseif ($parser->extS5Hooks == 'parse2')
-        {
-            $parser->setHook('slideshow', 'S5SlideShow::empty_tag_hook');
-            $parser->setHook('slide', 'S5SlideShow::empty_tag_hook');
-            $parser->setHook('slides', array($parser->extS5, 'slides_parse'));
-            $parser->setHook('slidecss', array($parser->extS5, 'slidecss_parse'));
-        }
-        return true;
-    }
+	// Setup parser hooks for S5
+	static function ParserFirstCallInit( $parser ) {
+		if ( !isset( $parser->extS5Hooks ) ) {
+			$parser->setHook( 'slideshow', 'S5SlideShow::slideshow_view' );
+			$parser->setHook( 'slide', 'S5SlideShow::slideshow_legacy' );
+			$parser->setHook( 'slides', 'S5SlideShow::slides_view' );
+			$parser->setHook( 'slidecss', 'S5SlideShow::slidecss_view' );
+		} elseif ( $parser->extS5Hooks == 'parse' ) {
+			$parser->setHook( 'slideshow', [ $parser->extS5, 'slideshow_parse' ] );
+			$parser->setHook( 'slide', [ $parser->extS5, 'slideshow_parse' ] );
+			$parser->setHook( 'slides', 'S5SlideShow::empty_tag_hook' );
+			$parser->setHook( 'slidecss', 'S5SlideShow::empty_tag_hook' );
+		} elseif ( $parser->extS5Hooks == 'parse2' ) {
+			$parser->setHook( 'slideshow', 'S5SlideShow::empty_tag_hook' );
+			$parser->setHook( 'slide', 'S5SlideShow::empty_tag_hook' );
+			$parser->setHook( 'slides', [ $parser->extS5, 'slides_parse' ] );
+			$parser->setHook( 'slidecss', [ $parser->extS5, 'slidecss_parse' ] );
+		}
+		return true;
+	}
 
-    // Setup hook for image scaling hack
-    static function Setup()
-    {
-        global $egS5BrowserScaleHack, $wgHooks;
-        if ($egS5BrowserScaleHack)
-            $wgHooks['ImageBeforeProduceHTML'][] = 'S5SlideShowHooks::ImageBeforeProduceHTML';
-    }
+	// Setup hook for image scaling hack
+	static function Setup() {
+		global $egS5BrowserScaleHack, $wgHooks;
+		if ( $egS5BrowserScaleHack ) {
+			$wgHooks['ImageBeforeProduceHTML'][] = 'S5SlideShowHooks::ImageBeforeProduceHTML';
+		}
+	}
 
-    // Hook that creates {{S5SLIDESHOW}} magic word
-    static function MagicWordwgVariableIDs(&$mVariablesIDs)
-    {
-        $mVariablesIDs[] = 's5slideshow';
-        return true;
-    }
+	// Hook that creates {{S5SLIDESHOW}} magic word
+	static function MagicWordwgVariableIDs( &$mVariablesIDs ) {
+		$mVariablesIDs[] = 's5slideshow';
+		return true;
+	}
 
-    // Hook that evaluates {{S5SLIDESHOW}} magic word
-    static function ParserGetVariableValueSwitch($parser, $varCache, $index, &$ret)
-    {
-        if ($index == 's5slideshow')
-            $ret = empty(self::$parsingSlide) ? '' : '1';
-        return true;
-    }
+	// Hook that evaluates {{S5SLIDESHOW}} magic word
+	static function ParserGetVariableValueSwitch( $parser, $varCache, $index, &$ret ) {
+		if ( $index == 's5slideshow' ) {
+			$ret = empty( self::$parsingSlide ) ? '' : '1';
+		}
+		return true;
+	}
 
-    // Render pictures differently in slide show mode
-    static function ImageBeforeProduceHTML($skin, $title, $file, $frameParams, $handlerParams, $time, &$res)
-    {
-        global $wgVersion;
-        if (empty(self::$parsingSlide) || !$file || !$file->exists() || !isset($handlerParams['width']))
-            return true;
-        $fp = &$frameParams;
-        $hp = &$handlerParams;
-        $center = false;
-        if (isset($fp['align']) && $fp['align'] == 'center')
-        {
-            $center = true;
-            $fp['align'] = 'none';
-        }
-        $thumb = $file->getUnscaledThumb(isset($hp['page']) ? array('page' => $hp['page']) : false);
-        $params = array(
-            'alt' => @$fp['alt'],
-            'title' => @$fp['title'],
-        );
-        if (version_compare($wgVersion, '1.22', '>='))
-        {
-            $params['override-height'] = ceil($thumb->getHeight() * $hp['width'] / $thumb->getWidth());
-            $params['override-width'] = $hp['width'];
-        }
-        else
-        {
-            $thumb->height = ceil($thumb->height * $hp['width'] / $thumb->width);
-            $thumb->width = $hp['width'];
-        }
-        if (!empty($fp['link-url']))
-            $params['custom-url-link'] = $fp['link-url'];
-        elseif (!empty($fp['link-title']))
-            $params['custom-title-link'] = $fp['link-title'];
-        elseif (!empty($fp['no-link']))
-        {
-        }
-        else
-            $params['desc-link'] = true;
-        $res .= $thumb->toHtml($params);
-        if (isset($fp['thumbnail']))
-        {
-            $outerWidth = $thumb->getWidth()+2;
-            $res = "<div class=\"thumb t$fp[align]\" style='border:0'>".
-                "<div class=\"thumbinner\">$res</div><div class='thumbcaption'>$fp[caption]</div></div>";
-        }
-        if (isset($fp['align']) && $fp['align'])
-            $res = "<div class=\"float$fp[align]\">$res</div>";
-        if ($center)
-            $res = "<div class=\"center\">$res</div>";
-        return false;
-    }
+	// Render pictures differently in slide show mode
+	static function ImageBeforeProduceHTML( $skin, $title, $file, $frameParams, $handlerParams, $time, &$res ) {
+		global $wgVersion;
+		if ( empty( self::$parsingSlide ) || !$file || !$file->exists() || !isset( $handlerParams['width'] ) ) {
+			return true;
+		}
+		$fp = &$frameParams;
+		$hp = &$handlerParams;
+		$center = false;
+		if ( isset( $fp['align'] ) && $fp['align'] == 'center' ) {
+			$center = true;
+			$fp['align'] = 'none';
+		}
+		$thumb = $file->getUnscaledThumb( isset( $hp['page'] ) ? [ 'page' => $hp['page'] ] : false );
+		$params = [
+			'alt' => @$fp['alt'],
+			'title' => @$fp['title'],
+		];
+		if ( version_compare( $wgVersion, '1.22', '>=' ) ) {
+			$params['override-height'] = ceil( $thumb->getHeight() * $hp['width'] / $thumb->getWidth() );
+			$params['override-width'] = $hp['width'];
+		} else {
+			$thumb->height = ceil( $thumb->height * $hp['width'] / $thumb->width );
+			$thumb->width = $hp['width'];
+		}
+		if ( !empty( $fp['link-url'] ) ) {
+			$params['custom-url-link'] = $fp['link-url'];
+  } elseif ( !empty( $fp['link-title'] ) ) {
+			$params['custom-title-link'] = $fp['link-title'];
+  } elseif ( !empty( $fp['no-link'] ) ) {
+  } else { 			$params['desc-link'] = true;
+  }
+		$res .= $thumb->toHtml( $params );
+		if ( isset( $fp['thumbnail'] ) ) {
+			$outerWidth = $thumb->getWidth() + 2;
+			$res = "<div class=\"thumb t$fp[align]\" style='border:0'>" .
+				"<div class=\"thumbinner\">$res</div><div class='thumbcaption'>$fp[caption]</div></div>";
+		}
+		if ( isset( $fp['align'] ) && $fp['align'] ) {
+			$res = "<div class=\"float$fp[align]\">$res</div>";
+		}
+		if ( $center ) {
+			$res = "<div class=\"center\">$res</div>";
+		}
+		return false;
+	}
 
-    // Hook for ?action=slide
-    static function UnknownAction($action, $article)
-    {
-        global $wgMaxRedirects, $wgRequest;
-        if ($action == 'slide')
-        {
-            $s5skin = trim($wgRequest->getVal('s5skin'));
-            if (preg_match('/[^\w-]/', $s5skin))
-                $s5skin = '';
-            $print = $wgRequest->getVal('print');
-            if ($print)
-            {
-                preg_match_all('/\d+/s', $print, $print, PREG_PATTERN_ORDER);
-                $print = $print[0];
-            }
-            else
-                $print = false;
-            if ($wgRequest->getVal('s5css'))
-            {
-                // Get CSS for a given S5 style (from wiki-pages)
-                S5SlideShow::genStyle($s5skin, $print);
-                return false;
-            }
-            // Check if the article is readable
-            $title = $article->getTitle();
-            for ($r = 0; $r < $wgMaxRedirects && $title->isRedirect(); $r++)
-            {
-                if (!$title->userCan('read'))
-                    return true;
-                $title = $article->followRedirect();
-                $article = new Article($title);
-            }
-            // Hack for CustIS live preview
-            // TODO remove support for loading text from session object and
-            //      replace it by support for save-staying-in-edit-mode extension
-            $content = $wgRequest->getVal('wpTextbox1');
-            if (!$content && ($t1 = $wgRequest->getSessionData('wpTextbox1')))
-            {
-                $content = $t1;
-                $wgRequest->setSessionData('wpTextbox1', NULL);
-            }
-            // Generate presentation HTML content
-            $slideShow = new S5SlideShow($title, $content);
-            if ($s5skin)
-                $slideShow->attr['style'] = $s5skin;
-            $slideShow->genSlideFile($print);
-            return false;
-        }
-        return true;
-    }
+	// Hook for ?action=slide
+	static function UnknownAction( $action, $article ) {
+		global $wgMaxRedirects, $wgRequest;
+		if ( $action == 'slide' ) {
+			$s5skin = trim( $wgRequest->getVal( 's5skin' ) );
+			if ( preg_match( '/[^\w-]/', $s5skin ) ) {
+				$s5skin = '';
+			}
+			$print = $wgRequest->getVal( 'print' );
+			if ( $print ) {
+				preg_match_all( '/\d+/s', $print, $print, PREG_PATTERN_ORDER );
+				$print = $print[0];
+			} else { 				$print = false;
+			}
+			if ( $wgRequest->getVal( 's5css' ) ) {
+				// Get CSS for a given S5 style (from wiki-pages)
+				S5SlideShow::genStyle( $s5skin, $print );
+				return false;
+			}
+			// Check if the article is readable
+			$title = $article->getTitle();
+			for ( $r = 0; $r < $wgMaxRedirects && $title->isRedirect(); $r++ ) {
+				if ( !$title->userCan( 'read' ) ) {
+					return true;
+				}
+				$title = $article->followRedirect();
+				$article = new Article( $title );
+			}
+			// Hack for CustIS live preview
+			// TODO remove support for loading text from session object and
+			//      replace it by support for save-staying-in-edit-mode extension
+			$content = $wgRequest->getVal( 'wpTextbox1' );
+			if ( !$content && ( $t1 = $wgRequest->getSessionData( 'wpTextbox1' ) ) ) {
+				$content = $t1;
+				$wgRequest->setSessionData( 'wpTextbox1', null );
+			}
+			// Generate presentation HTML content
+			$slideShow = new S5SlideShow( $title, $content );
+			if ( $s5skin ) {
+				$slideShow->attr['style'] = $s5skin;
+			}
+			$slideShow->genSlideFile( $print );
+			return false;
+		}
+		return true;
+	}
 
-    // Used to display CSS files on S5 skin CSS pages when they don't exist
-    static function ArticleFromTitle($title, &$article)
-    {
-        if ($title->getNamespace() == NS_MEDIAWIKI &&
-            preg_match('#^S5/([\w-]+)/((core|base|framing|pretty).css)$#s', $title->getText(), $m))
-        {
-            $file = dirname(__FILE__).'/'.str_replace('$skin', $m[1], self::$styles[$m[2]]);
-            if (file_exists($file))
-            {
-                $article = new S5SkinArticle($title, $m[1], $file);
-                return false;
-            }
-        }
-        return true;
-    }
+	// Used to display CSS files on S5 skin CSS pages when they don't exist
+	static function ArticleFromTitle( $title, &$article ) {
+		if ( $title->getNamespace() == NS_MEDIAWIKI &&
+			preg_match( '#^S5/([\w-]+)/((core|base|framing|pretty).css)$#s', $title->getText(), $m ) ) {
+			$file = __DIR__ . '/' . str_replace( '$skin', $m[1], self::$styles[$m[2]] );
+			if ( file_exists( $file ) ) {
+				$article = new S5SkinArticle( $title, $m[1], $file );
+				return false;
+			}
+		}
+		return true;
+	}
 
-    // Used to display CSS files on S5 skin CSS pages in edit mode
-    static function AlternateEdit($editpage)
-    {
-        if ($editpage->mArticle instanceof S5SkinArticle &&
-            !$editpage->mArticle->exists())
-            $editpage->mPreloadText = $editpage->mArticle->getContent();
-        return true;
-    }
+	// Used to display CSS files on S5 skin CSS pages in edit mode
+	static function AlternateEdit( $editpage ) {
+		if ( $editpage->mArticle instanceof S5SkinArticle &&
+			!$editpage->mArticle->exists() ) {
+			$editpage->mPreloadText = $editpage->mArticle->getContent();
+		}
+		return true;
+	}
 }
